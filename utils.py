f5316eab2655c29361b9d25633b9508e30650c24
Aman Kumar <46129975+amanjain487@users.noreply.github.com>
2021-06-29 19:05:45 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -42,7 +42,7 @@ def train_transform(train):
   if "horizontal_flip" in train:
     train_list.append(transforms.RandomHorizontalFlip())
   if "cutout" in train:
-    albumentation_train_list.append(A.CoarseDropout(p=0.5, max_holes = 1, max_height=16, max_width=16, min_holes = 1, min_height=16, min_width=16, fill_value=(mean), mask_fill_value = None))
+    albumentation_train_list.append(A.CoarseDropout(p=0.5, max_holes = 1, max_height=16, max_width=16, min_holes = 1, min_height=16, min_width=16, fill_value=(0.4914, 0.4822, 0.4465), mask_fill_value = None))
   if "shift_scale_rotate" in train:
      albumentation_train_list.append(A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=15, p=0.5))
   if "grayscale" in train:
