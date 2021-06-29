dc2e7a4e3be011100933af0fd3331387e1a06dc4
Aman Kumar <46129975+amanjain487@users.noreply.github.com>
2021-06-29 18:57:09 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,18 +7,60 @@ import torch.backends.cudnn as cudnn
 import torchvision
 import torchvision.transforms as transforms
 
-
-def train_transform():
-  return transforms.Compose([
-    transforms.ToTensor(),
-      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-      transforms.RandomCrop(32, padding=4),
-      transforms.RandomHorizontalFlip()
-  ])
+!pip install git+https://github.com/albumentations-team/albumentations.git
+import albumentations as A
 
 
-def test_transform():
-  return transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+class CIFAR_10_Dataset(torch.utils.data.Dataset):
+  def __init__(self,  dataset, transformer=None):
+        self.dataset = dataset
+        self.transforms = transformer
+  def __len__(self):
+        return len(self.dataset)
+  def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        img, target = self.dataset[idx]
+        img = img.cpu().detach().numpy()
+        img = np.asarray(img).reshape((32,32,3))
+        if self.transforms is not None:
+            image = self.transforms(image=img)
+        img = torch.from_numpy(img.reshape(3,32,32))
+        return img, target
+
+
+def train_transform(train):
+  albumentation_train_list = []
+  train_list = []
+  if "totensor" in train:
+    train_list.append()
+  if "normalize_normal" in train:
+    train_list.append()
+  if "normalize_mean" in train:
+    train_list.append()
+  if "randomcrop" in train:
+    train_list.append()
+  if "horizontal_flip" in train:
+    train_list.append()
+  if "cutout" in train:
+    albumentation_train_list.append(A.CoarseDropout(p=0.5, max_holes = 1, max_height=16, max_width=16, min_holes = 1, min_height=16, min_width=16, fill_value=(mean), mask_fill_value = None))
+  if "shift_scale_rotate" in train:
+     albumentation_train_list.append(A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=15, p=0.5))
+  if "grayscale" in train:
+     albumentation_train_list.append(A.ToGray(p=0.5))
+  
+  return transforms.Compose(train_list), A.Compose(albumentation_train_list)
+
+
+
+def load_dataset(tensor_train, numpy_train):
+  train_dataset = CIFAR_10_Dataset(datasets.CIFAR10(root='./data', train=True, download=True,
+                                  transform=tensor_train), numpy_train)
+
+  testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transforms.Compose([transforms.ToTensor(),
+                                                                                    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                                                                                   ]))
+  return train_dataset, testset
+
+
