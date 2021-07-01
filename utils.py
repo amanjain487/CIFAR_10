1df6906cc890a9002f49a902bc034d5739d63744
Aman Kumar <46129975+amanjain487@users.noreply.github.com>
2021-07-01 19:22:31 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -136,7 +136,7 @@ def plot_images(img_data, classes):
         img = denormalize(img_data[index-1]["img"])  # unnormalize
         plt.subplot(5, 5, index)
         plt.axis('off')
-        plt.imshow(np.transpose(img.cpu().numpy(), (1, 2, 0)))
+        plt.imshow(np.transpose(img.cpu().numpy().astype('uint8'), (1, 2, 0)))
         plt.title("Predicted: %s\nActual: %s" % (classes[img_data[index-1]["pred"]], classes[img_data[index-1]["target"]]))
 
     plt.tight_layout()
