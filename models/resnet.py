459f90d72e25117be12ada138cd2e271075d7cad
Aman Kumar <46129975+amanjain487@users.noreply.github.com>
2021-07-01 15:23:31 +0530

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -9,10 +9,10 @@ class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, stride=stride, padding=3, bias=False)
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.GroupNorm(1, planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=1, padding=3, bias=False)
+                               stride=1, padding=1, bias=False)
         self.bn2 = nn.GroupNorm(1, planes)
 
         self.shortcut = nn.Sequential()
@@ -44,7 +44,7 @@ class ResNet(nn.Module):
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=1)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
