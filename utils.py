4986515e3262e214ead580187958a906e6d2dc61
Aman Kumar <46129975+amanjain487@users.noreply.github.com>
2021-07-01 16:42:43 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -97,20 +97,13 @@ def denormalize(tensor, mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.20
   
 def identify_images(net, criterion, device, testloader):
     net.eval()
-    test_loss = 0
-    correct = 0
-    total = 0
     correct_images = []
     incorrect_images = []
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
             inputs, targets = inputs.to(device), targets.to(device)
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
-           
-            test_loss += loss.item()
+            outputs = net(inputs)           
             _, predicted = outputs.max(1)
-            total += targets.size(0)
             is_correct = pred.eq(targets.view_as(predicted))
             
             misclassified_inds = (is_correct==0).nonzero()[:,0]
@@ -132,10 +125,7 @@ def identify_images(net, criterion, device, testloader):
                     "pred": pred[ind][0].cpu().numpy(),
                     "img": data[ind]
                 })
-            
-            correct += predicted.eq(targets).sum().item()
-    print('Test Loss: %.3f | Test Acc: %.3f%% (%d/%d)' % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
-
+  return correct_images, incorrect_images
   
   
 def plot_images(img_data, classes):
