3fbe34965f2f6092f04c2a68e9bfebd19c688f8e
Aman Kumar <46129975+amanjain487@users.noreply.github.com>
2021-07-02 15:56:36 +0530

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -39,12 +39,12 @@ class ResNet(nn.Module):
         self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-                               stride=1, padding=2, bias=False)
+                               stride=1, padding=1, bias=False)
         self.bn1 = nn.GroupNorm(1, 64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=1)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
